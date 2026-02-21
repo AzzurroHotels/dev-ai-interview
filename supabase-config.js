2e0da2960ba53ffc73294e8725e0c6767a81751a
AzzurroHotels <alvinrustia@azzurrohotels.com>
2026-02-21 13:43:34 +1100

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -4,4 +4,4 @@ export const SUPABASE_URL = "https://vsvimpbbjzasgkaisbqk.supabase.co";
 export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZzdmltcGJianphc2drYWlzYnFrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE2MTExODMsImV4cCI6MjA4NzE4NzE4M30.tfaZ4JZkj40PNVs3GWU4B3VwvlJK4VVmgwkiW-f9lFw";
 
 // Email destination for interview submissions
-export const CAREERS_EMAIL = "careers@cbit.com";
+export const CAREERS_EMAIL = "careers@azzurrohotels.com";
